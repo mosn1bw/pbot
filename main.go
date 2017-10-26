ebacfad31d72c6dfaad9db36c55ad7be5bd6cda8
Yikaros <jet113102@gmail.com>
2017-10-26 10:32:55 +0800

    Update main.go

--- main.go
+++ main.go
@@ -47,12 +47,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
-			switch message := event.Message.(type) {
-			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(": OK!")).Do(); err != nil {
-					log.Print(err)
-				}
-			}
+			bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(": OK!")).Do()
 		}
 	}
 }
