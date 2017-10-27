251a9f3e9507765be423cc737b777ac4e8bebc3b
Yikaros <jet113102@gmail.com>
2017-10-27 13:05:30 +0800

    Update main.go

--- main.go
+++ main.go
@@ -125,23 +125,21 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 								msg = "拍謝啦! 我是笨笨的電腦，不知道您要哪種石斑捏，我們有龍虎石斑、青斑、還有珍珠石斑"
 						}
 						i:=0
-						if food != ""{
-							for i<=len(list_array){
-								var menu []string
-								menu = strings.Split(list_array[i], " ")
-								if menu[0] == food{
-									price=menu[1]
-									stock=menu[2]
-									break
-								}
-								i++
+						for i<=len(list_array){
+							var menu []string
+							menu = strings.Split(list_array[i], " ")
+							if menu[0] == food{
+								price=menu[1]
+								stock=menu[2]
+								break
 							}
-							switch{
-								case Contains(message.Text,"多少錢")||Contains(message.Text,"怎麼賣")||Contains(message.Text,"怎麼算"):
-									msg = food + "一斤" + price
-								case Contains(message.Text,"還有多少")||Contains(message.Text,"剩下多少")||Contains(message.Text,"庫存")||Contains(message.Text,"還有幾"):
-									msg = food + "大概還有" + stock + "尾可以買，賣完就沒了喔!! 趕快來電088953096/0939220743黃先生" 
-							}
+							i++
+						}
+						switch{
+							case Contains(message.Text,"多少錢")||Contains(message.Text,"怎麼賣")||Contains(message.Text,"怎麼算"):
+								msg = food + "一斤" + price
+							case Contains(message.Text,"還有多少")||Contains(message.Text,"剩下多少")||Contains(message.Text,"庫存")||Contains(message.Text,"還有幾"):
+								msg = food + "大概還有" + stock + "尾可以買，賣完就沒了喔!! 趕快來電088953096/0939220743黃先生" 
 						}
 						if(msg != "")&&(food != ""){
 							bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(msg)).Do() 
