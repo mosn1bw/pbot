4a5efdf9220a5b5e6f995eaa089e40d3b4652d94
Yikaros <jet113102@gmail.com>
2017-11-02 10:57:39 +0800

    Update main.go

--- main.go
+++ main.go
@@ -115,7 +115,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 								case Contains(message.Text,"一斤多少")||Contains(message.Text,"多少錢")||Contains(message.Text,"怎麼賣")||Contains(message.Text,"怎麼算"):
 									bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(food + "一斤" + price)).Do() 
 								case Contains(message.Text,"要買"):
-									bot.PushMessage(admin,"有人要買菜").Do() 
+									bot.PushMessage(admin,linebot.NewTextMessage("有人要買菜")).Do() 
 							}
 						}
 //石斑魚的code
