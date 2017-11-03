42e19329b77b4430ec0bb96fc97c0c5c5b13d4f5
Yikaros <jet113102@gmail.com>
2017-11-03 15:42:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -220,13 +220,13 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 								}
 								i++
 							}
-							if !Contains(message.Text,"怎麼算是"){
-								switch{
-									case Contains(message.Text,"一斤多少")||Contains(message.Text,"多少錢")||Contains(message.Text,"怎麼賣")||Contains(message.Text,"怎麼算"):						
-										bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(food + "一斤" + price)).Do() 
-									case Contains(message.Text,"還有多少")||Contains(message.Text,"剩下多少")||Contains(message.Text,"庫存")||Contains(message.Text,"還有幾"):
-										bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(food + "大概還有" + stock + "尾可以買，賣完就沒了喔!! 趕快來電088953096/0939220743黃先生")).Do() 
-								}
+							switch{
+								case Contains(message.Text,"怎麼算是"):
+									
+								case Contains(message.Text,"一斤多少")||Contains(message.Text,"多少錢")||Contains(message.Text,"怎麼賣")||Contains(message.Text,"怎麼算"):
+									bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(food + "一斤" + price)).Do() 
+								case Contains(message.Text,"還有多少")||Contains(message.Text,"剩下多少")||Contains(message.Text,"庫存")||Contains(message.Text,"還有幾"):
+									bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(food + "大概還有" + stock + "尾可以買，賣完就沒了喔!! 趕快來電088953096/0939220743黃先生")).Do() 
 							}
 	//以下是喇賽的code
 						case Contains(message.Text,"87"):
