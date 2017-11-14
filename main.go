bd4431e99a307200e298a735c9ec449bb208c259
Yikaros <jet113102@gmail.com>
2017-11-14 10:51:46 +0800

    Update main.go

--- main.go
+++ main.go
@@ -182,8 +182,8 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 							bot.ReplyMessage(event.ReplyToken, messgage).Do() **/
 						case Contains(message.Text,"尻槍"):
 							//影片總數，兩個要一起改
-							av_count := 6
-							var av_rnd [6]int
+							av_count := 8
+							var av_rnd [8]int
 							
 							av_rnd[0] = r.Intn(av_count)+1
 							s:=1
